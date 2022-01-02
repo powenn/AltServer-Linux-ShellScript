ccb463120cf47c70fea4836a89624010db2f7626
powenn <powen0966091716@gmail.com>
2022-01-02 10:53:54 +0800

    add

--- setup.sh
+++ setup.sh
@@ -11,11 +11,15 @@ mkdir AltServer
 cd AltServer
 wget https://raw.githubusercontent.com/powenn/AltServer-Linux-ShellScript/main/update.sh
 wget https://raw.githubusercontent.com/powenn/AltServer-Linux-ShellScript/main/start.sh
+wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/install.sh
+wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/daemon.sh
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltServer
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
 chmod +x AltServer
 chmod +x start.sh
 chmod +x update.sh
+chmod +x install.sh
+chmod +x daemon.sh
 mkdir ipa
 echo "Finished"
 echo "run 'cd AltServer' and ./start.sh to start "
--- update.sh
+++ update.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-rm -rf AltServer start.sh update.sh AltStore.ipa
+rm -rf AltServer start.sh update.sh AltStore.ipa daemon.sh install.sh
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltServer
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/start.sh
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/update.sh
+wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/install.sh
+wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/daemon.sh
 wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
 
 chmod +x AltServer
 chmod +x update.sh
 chmod +x start.sh
+chmod +x install.sh
+chmod +x daemon.sh
 
 echo "Update Finished"
 exit
