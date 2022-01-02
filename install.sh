57c36c26920be45086cde1ee7ea6ae69082d753b
powenn <powen0966091716@gmail.com>
2022-01-02 17:09:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,17 +20,19 @@ read AppleID
 
 echo "Please provide the password of AppleID"
 read password
-
+rm -rf TempAltStore.ipa Payload
 unzip AltStore.ipa
-plistutil -i ./Payload/AltStore.app/Info.plist -o ./Payload/AltStore.app/temp.txt
-sed -i 's/'00008030-001948590202802E'/'${UDID}'/' ./Payload/AltStore.app/temp.txt
-plistutil -i ./Payload/AltStore.app/temp.txt -o ./Payload/AltStore.app/Info.plist
-rm -rf ./Payload/AltStore.app/temp.txt
+cd ./Payload/AltStore.app
+plistutil -i ./Info.plist -o ./temp.txt
+sed -i 's/00008030-001948590202802E/'${UDID}'/' ./temp.txt
+plistutil -i ./temp.txt -o ./Info.plist
+rm -rf ./temp.txt
+cd ../..
 zip -qr TempAltStore.ipa Payload
 PATH=./TempAltStore.ipa
 
 ./AltServer -u "${UDID}" -a "$AppleID" -p "$password" "$PATH"
-rm -rf TempAltStore.ipa Payload
+
 echo "Finished"
 echo "To use AltServer as daemon,run ./daemon.sh"
 exit
