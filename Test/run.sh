58715f16af056303458cdf23c45b944624534a0b
powenn <powen0966091716@gmail.com>
2022-01-12 21:58:28 +0800

    test

--- Test/run.sh
+++ Test/run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-
-set -x
-
 echo "Checking source"
 if [ ! -e "AltStore.ipa" ]; then
     wget https://github.com/powenn/AltServer-Linux-ShellScript/blob/main/AltStore.ipa
@@ -10,68 +7,179 @@ elif [ ! -e "ipa" ]; then
     mkdir ipa
 elif [ ! -e "saved.txt" ]; then
     touch saved.txt
+elif [ "stat AltServer | grep -rw-r--r--" != "" ] ; then
+    chmod +x AltServer
 fi
 
-case "$1" in
-  -i|--Install-AltStore )
-for job in `jobs -p`
-do
-  wait $job
-done
-echo "It will install AltStore to your device"
-echo "Please connect to your device and press Enter to continue"
-read key
-idevicepair pair
-UDID=$(lsusb -v 2> /dev/null | grep -e "Apple Inc" -A 2 | grep iSerial | awk '{print $3}')
-echo "This is the UDID of your device"
-echo "${UDID}"
-echo "Please provide your AppleID"
-read AppleID
-echo "Please provide the password of AppleID"
-read password
-PATH=./AltStore.ipa
-
-echo "Finished"
-ExistAccount=$(grep "AppleID:'$AppleID',PASSWORD:'$password'" saved.txt)
-if [ ${ExistID}="" ]; then
-    echo "Do you want to save this AppleID ? [y/n]"
-elif [ ${ExistID}=${ExistID} ]; then
-    break
-fi
-read ans
-case "$ans" in
-    [yY][eE][sS]|[yY] )
-    cat "AppleID:$AppleID,PASSWORD:$password" >> ./saved.txt
-    break
-    ;;
-    [nN][oO]|[nN] )
-    break
-    ;;
-    * )
-    ;;
-esac
-    ;;
-  * )
-    cat << EOF
+    cat << EOF >help.txt
 #####################################
 #                                   #
 #  Welcome to the AltServer script  #
 #                                   #
 #####################################
 
-Usage: ./runsh [OPTION]
+Usage: [OPTION]
 
 OPTIONS
 
-  -i, --Install AltStore
+  1, --Install AltStore
     Install AltStore to your device
-  -a, --Install ipa
+  2, --Install ipa
     Install ipa in Folder 'ipa',make sure you have put ipa files in the Folder before run this
-  -d, --Daemon mode
+  3, --Daemon mode
     Switch to Daemon mode to refresh apps or AltStore
-  -h, --help
-    Show this message ;)
+  h, --help
+    Show this message
     
 EOF
+
+cat help.txt
+
+echo "Enter OPTION to continue"
+read option
+
+case "$option" in
+  1|--Install-AltStore )
+for job in `jobs -p`
+do
+  wait $job
+done
+
+echo "It will install AltStore to your device"
+echo "Please connect to your device and press Enter to continue"
+read key
+idevicepair pair
+
+ExistAccount=$(cat saved.txt)
+
+if [ "$ExistAccount" != "" ]; then
+    echo "Do you want to use saved Account ? [y/n]"
+    read reply
+    case "$reply" in
+        [yY][eE][sS]|[yY] )
+        echo "Which account you want to use ? "
+        nl saved.txt
+        echo "please enter the number "
+        read number
+        ExistID=$(sed -n "$number"p saved.txt | cut -d , -f 1)
+        ExistPasswd=$(sed -n "$number"p saved.txt | cut -d , -f 2)
+        ;;
+        [nN][oO]|[nN] )
+        echo "Please provide your AppleID"
+        read AppleID
+        echo "Please provide the password of AppleID"
+        read password
+        ;;
+        * )
+        ;;
+    esac
+fi
+
+UDID=$(lsusb -v 2> /dev/null | grep -e "Apple Inc" -A 2 | grep iSerial | awk '{print $3}')
+Account=$AppleID,$password
+CheckAccount=$(grep $Account saved.txt)
+
+PATH=./AltStore.ipa
+if [ "$ExistID" == "" ]; then
+    ./AltServer -u "${UDID}" -a "$AppleID" -p "$password" "$PATH"
+elif [ "$ExistAccount" != "" ]; then
+    ./AltServer -u "${UDID}" -a "$ExistID" -p "$ExistPasswd" "$PATH"
+fi
+echo "Finished"
+if [ "$CheckAccount" == "" ] ; then
+    echo "Do you want to save this Account ? [y/n]"
+else
+    exit
+fi
+read ans
+case "$ans" in
+    [yY][eE][sS]|[yY] )
+    echo "$Account" >> saved.txt
+    echo "saved"
+    ;;
+    [nN][oO]|[nN] )
+    ;;
+    * )
+    ;;
+esac
+    ;;
+  2|--Install-ipa )
+for job in `jobs -p`
+do
+  wait $job
+done
+
+echo "Please connect to your device and press Enter to continue"
+read key
+idevicepair pair
+
+ExistAccount=$(cat saved.txt)
+
+if [ "$ExistAccount" != "" ]; then
+    echo "Do you want to use saved Account ? [y/n]"
+    read reply
+    case "$reply" in
+        [yY][eE][sS]|[yY] )
+        echo "Which account you want to use ? "
+        nl saved.txt
+        echo "please enter the number "
+        read number
+        ExistID=$(sed -n "$number"p saved.txt | cut -d , -f 1)
+        ExistPasswd=$(sed -n "$number"p saved.txt | cut -d , -f 2)
+        ;;
+        [nN][oO]|[nN] )
+        echo "Please provide your AppleID"
+        read AppleID
+        echo "Please provide the password of AppleID"
+        read password
+        ;;
+        * )
+        ;;
+    esac
+fi
+
+UDID=$(lsusb -v 2> /dev/null | grep -e "Apple Inc" -A 2 | grep iSerial | awk '{print $3}')
+Account=$AppleID,$password
+CheckAccount=$(grep $Account saved.txt)
+
+echo "Please provide the name of ipa (example: powen.ipa)"
+ls ./ipa
+PATH=./ipa/$name
+if [ "$ExistID" == "" ]; then
+    ./AltServer -u "${UDID}" -a "$AppleID" -p "$password" "$PATH"
+elif [ "$ExistAccount" != "" ]; then
+    ./AltServer -u "${UDID}" -a "$ExistID" -p "$ExistPasswd" "$PATH"
+fi
+echo "Finished"
+if [ "$CheckAccount" == "" ] ; then
+    echo "Do you want to save this Account ? [y/n]"
+else
+    exit
+fi
+read ans
+case "$ans" in
+    [yY][eE][sS]|[yY] )
+    echo "$Account" >> saved.txt
+    echo "saved"
+    ;;
+    [nN][oO]|[nN] )
+    ;;
+    * )
+    ;;
+esac
+    ;;
+  3|--Daemon-mode )
+for job in `jobs -p`
+do
+  wait $job
+done
+
+echo "Please connect to your device and press Enter to continue"
+read key
+idevicepair pair
+./AltServer
+    ;;
+  h|--help )
+cat help.txt
     ;;
 esac
