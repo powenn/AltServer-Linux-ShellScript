f1b4ea479cbaffb0ade2909c7b4023cc43605009
powenn <powen0966091716@gmail.com>
2022-01-16 21:32:17 +0800

    Initial commit

---
+++ AltServer-armv7/run.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+# Author of the script : powen
+
+# Check source and permission
+cd "$(dirname "$0")"
+echo "Checking source"
+if [[ ! -e "AltStore.ipa" ]]; then
+    wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
+fi
+if [[ ! -e "ipa" ]]; then
+    mkdir ipa
+fi
+if [[ ! -e "saved.txt" ]]; then
+    touch saved.txt
+fi
+if [[ "stat AltServer | grep -rw-r--r--" != "" ]] ; then
+    chmod +x AltServer
+fi
+
+#####
+HasExistAccount=$(cat saved.txt)
+UDID=$(lsusb -v 2> /dev/null | grep -e "Apple Inc" -A 2 | grep iSerial | awk '{print $3}')
+HasExistipa=$(ls ipa)
+
+
+# Instruction
+cat << EOF >help.txt
+    
+#####################################
+#  Welcome to the AltServer script  #
+#####################################
+
+Usage: [OPTION]
+
+OPTIONS
+
+  1, --Install AltStore
+    Install AltStore to your device
+  2, --Install ipa
+    Install ipa in Folder 'ipa',make sure you have put ipa files in the Folder before run this
+  d, --Daemon mode
+    Switch to Daemon mode to refresh apps or AltStore
+  e, --exit
+    Exit script
+  h, --help
+    Show this message
+    
+EOF
+
+# Print AltServer icon
+AltServerIcon() {
+    printf "\e[49m       \e[38;5;73;49m▄▄\e[38;5;66;49m▄\e[38;5;66;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;66;49m▄\e[38;5;73;49m▄▄\e[49m       \e[m
+\e[49m     \e[38;5;73;49m▄\e[38;5;66;48;5;73m▄\e[38;5;66;48;5;66m▄▄▄▄▄▄\e[38;5;67;48;5;66m▄▄\e[38;5;66;48;5;66m▄▄▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;73;49m▄\e[49m     \e[m
+\e[49m   \e[38;5;73;49m▄\e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;109;48;5;73m▄\e[38;5;109;48;5;109m▄▄▄▄\e[38;5;109;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[38;5;73;49m▄\e[49m   \e[m
+\e[49m \e[38;5;73;49m▄\e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[38;5;73;49m▄\e[49m \e[m
+\e[49m \e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[38;5;15;48;5;15m▄▄\e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;30;48;5;30m▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[49m \e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄\e[38;5;73;48;5;30m▄▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[38;5;15;48;5;15m▄\e[48;5;15m    \e[38;5;15;48;5;15m▄\e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;30m▄▄\e[38;5;30;48;5;30m▄▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄\e[38;5;66;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[48;5;15m          \e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;66;48;5;30m▄\e[38;5;30;48;5;30m▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄\e[38;5;30;48;5;66m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[48;5;15m          \e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;30;48;5;66m▄\e[38;5;30;48;5;30m▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄\e[38;5;6;48;5;66m▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[38;5;15;48;5;15m▄\e[48;5;15m    \e[38;5;15;48;5;15m▄\e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;72m▄\e[38;5;6;48;5;6m▄▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[49m \e[38;5;73;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[38;5;15;48;5;15m▄▄\e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄\e[38;5;73;48;5;6m▄\e[49m \e[m
+\e[49m \e[49;38;5;66m▀\e[38;5;67;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;67;48;5;6m▄\e[49;38;5;66m▀\e[49m \e[m
+\e[49m   \e[49;38;5;67m▀\e[38;5;66;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;6m▄\e[49;38;5;67m▀\e[49m   \e[m
+\e[49m     \e[49;38;5;66m▀\e[38;5;66;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;6;48;5;30m▄▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;6m▄\e[49;38;5;66m▀\e[49m     \e[m
+\e[49m       \e[49;38;5;66m▀▀\e[49;38;5;6m▀\e[38;5;66;48;5;6m▄▄▄▄▄▄▄▄\e[49;38;5;6m▀\e[49;38;5;66m▀▀\e[49m       \e[m
+";
+}
+
+# Check if there exists saved account
+# Ask if want to use saved account
+AskAccount() {
+    if [[ "$HasExistAccount" != "" ]]; then
+        echo "Do you want to use saved Account ? [y/n]"
+        read reply
+        case "$reply" in
+        [yY][eE][sS]|[yY] )
+        echo "Which account you want to use ? "
+        UseExistAccount=1
+        nl saved.txt
+        echo "please enter the number "
+        read number
+        ExistID=$(sed -n "$number"p saved.txt | cut -d , -f 1)
+        ExistPasswd=$(sed -n "$number"p saved.txt | cut -d , -f 2)
+        ;;
+        [nN][oO]|[nN] )
+        UseExistAccount=0
+        ;;
+        esac
+    fi
+    if [[ "$HasExistAccount" == "" ]]; then
+        UseExistAccount=0
+    fi
+    if [[ $UseExistAccount = 0 ]]; then
+        echo "Please provide your AppleID"
+        read AppleID
+        echo "Please provide the password of AppleID"
+        read password
+    fi
+}
+
+# Execute AltServer
+# Check if this account existed before
+AltServer() {
+    if [[ $UseExistAccount = 1 ]]; then
+        ./AltServer -u ${UDID} -a $ExistID -p $ExistPasswd $PATH
+    fi
+    if [[ $UseExistAccount = 0 ]]; then
+        ./AltServer -u ${UDID} -a $AppleID -p $password $PATH
+    fi
+    if [[ "$CheckAccount" == "" ]] ; then
+        RunScript=1
+    fi
+    if [[ "$CheckAccount" != "" ]] ; then
+        exit
+    fi
+}
+
+# Check if there exists ipa files in ipa folder
+ipaCheck() {
+    if [[ "$HasExistipa" != "" ]]; then
+        echo "Please provide the number of ipa "
+        echo "$HasExistipa" > ipaList.txt
+        nl ipaList.txt
+        read ipanumber
+        Existipa=$(sed -n "$ipanumber"p ipaList.txt )
+    else
+        echo "There is no ipa filess in ipa folder "
+        exit
+    fi
+}
+
+# Ask to save the new account
+SaveAcccount() {
+    echo "Do you want to save this Account ? [y/n]"
+    read ans
+    case "$ans" in
+    [yY][eE][sS]|[yY] )
+    echo "$Account" >> saved.txt
+    echo "saved"
+    exit
+    ;;
+    [nN][oO]|[nN] )
+    exit
+    ;;
+    esac
+}
+
+# Start script
+AltServerIcon
+cat help.txt
+echo "Please connect to your device and press Enter to continue"
+read key
+idevicepair pair
+
+RunScript=0
+while [ $RunScript = 0 ] ; do
+    echo "Enter OPTION to continue"
+    read option
+    case "$option" in
+    
+  1|--Install-ipa )
+  
+    for job in `jobs -p`
+    do
+    wait $job
+    done
+
+    idevicepair pair
+    AskAccount
+    
+    Account=$AppleID,$password
+    CheckAccount=$(grep $Account saved.txt)
+    PATH=./AltStore.ipa
+    
+    AltServer
+    ;;
+    
+  2|--Install-ipa )
+  
+    for job in `jobs -p`
+    do
+    wait $job
+    done
+
+    idevicepair pair
+    ipaCheck
+    AskAccount
+
+    Account=$AppleID,$password
+    CheckAccount=$(grep $Account saved.txt)
+    PATH=./ipa/$Existipa
+    
+    AltServer
+    ;;
+        
+  d|--Daemon-mode )
+    for job in `jobs -p`
+    do
+    wait $job
+    done
+
+    idevicepair pair
+    ./AltServer
+    ;;
+  e|--exit )
+    exit
+    ;;
+  h|--help )
+    AltServerIcon
+    cat help.txt
+    ;;
+    esac
+
+done
+
+while [ $RunScript = 1 ] ; do
+    SaveAcccount
+done
---
+++ README.md
@@ -0,0 +1,68 @@
+# AltServer-Linux-ShellScript
+
+Works on debian 11 x64 !!
+
+And works on pinephone (tested on mobian)
+
+Demo videos at the bottom
+
+AltServer from https://github.com/NyaMisty/AltServer-Linux/releases
+
+Special thanks to [NyaMisty](https://github.com/NyaMisty) for AltServer-Linux project 
+
+## Features (now avaliable in pre-release)
+- account saving
+- enter number to select and use saved account
+- enter number to select ipa
+- only need one command to use the script `./run.sh`
+
+
+## Note 
+
+Just run `./run.sh` and follow the instruction
+
+***Not work on every linux distribution and architectures,report issues to [issues](https://github.com/powenn/AltServer-Linux-ShellScript/issues)***
+
+![photo][0]
+
+## Get start
+
+Get the release which support your device architecture
+
+You need idevicemobile
+
+`apt setup dependencies`
+```
+sudo apt-get install usbmuxd libimobiledevice6 libimobiledevice-utils
+```
+
+Please storage your ipa files into AltServer/ipa
+
+run `./run.sh` to start
+
+![photo][1]
+![photo][2]
+![photo][3]
+
+### Video(install AltStore)
+
+<a href="https://www.youtube.com/watch?v=eraWIbdxyOo">
+  <img src="https://img.youtube.com/vi/eraWIbdxyOo/maxresdefault.jpg" >
+</a>
+
+<a href="https://www.youtube.com/watch?v=57JDy2GX1JY">
+  <img src="https://img.youtube.com/vi/57JDy2GX1JY/maxresdefault.jpg" >
+</a>
+
+### Video(install ipa)
+
+<a href="https://www.youtube.com/watch?v=AgqoaBQd_p8">
+  <img src="https://img.youtube.com/vi/AgqoaBQd_p8/maxresdefault.jpg" >
+</a>
+
+
+
+[0]:https://github.com/powenn/AltServer-Linux-ShellScript/blob/main/photos/00.jpg
+[1]:https://github.com/powenn/AltServer-Linux-ShellScript/blob/main/photos/01.jpg
+[2]:https://github.com/powenn/AltServer-Linux-ShellScript/blob/main/photos/02.jpg
+[3]:https://github.com/powenn/AltServer-Linux-ShellScript/blob/main/photos/03.jpg
