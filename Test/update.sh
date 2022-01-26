4449af637a3f3378d25e78ea9385f16852b74747
powenn <powen0966091716@gmail.com>
2022-01-26 11:44:37 +0800

    fix

--- Test/update.sh
+++ Test/update.sh
@@ -2,13 +2,13 @@
 # Author of the script : powen
 
 cd "$(dirname "$0")"
-rm -rf AltStore.ipa
-wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
 
 LocalVersion=$(sed -n 1p version)
 LatestVersion=$(curl -Lsk 'https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/version')
 Arch=$(sed -n 2p version)
 if [[ $LatestVersion != $LocalVersion ]] ; then
+    rm -rf AltStore.ipa
+    wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
     cd ..
     wget https://github.com/powenn/AltServer-Linux-ShellScript/releases/download/$LatestVersion/AltServer-$Arch.zip
     unzip -o AltServer-$Arch.zip
