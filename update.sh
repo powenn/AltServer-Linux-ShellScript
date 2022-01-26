644f2780a3f8265da2feb38dde84a4dbfd793bdb
powenn <powen0966091716@gmail.com>
2022-01-26 17:29:10 +0800

    FIx

--- update.sh
+++ update.sh
@@ -2,17 +2,17 @@
 # Author of the script : powen
 
 cd "$(dirname "$0")"
-
 LocalVersion=$(sed -n 1p version)
 LatestVersion=$(curl -Lsk 'https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/version')
 Arch=$(sed -n 2p version)
+DIR=$(pwd)
 if [[ $LatestVersion != $LocalVersion ]] ; then
     rm -rf AltStore.ipa
     wget https://github.com/powenn/AltServer-Linux-ShellScript/raw/main/AltStore.ipa
-    cd ..
     wget https://github.com/powenn/AltServer-Linux-ShellScript/releases/download/$LatestVersion/AltServer-$Arch.zip
     unzip -o AltServer-$Arch.zip
-    rm -rf AltServer-$Arch.zip
+    cp -R ./AltServer-$Arch/* $DIR
+    rm -rf AltServer-$Arch.zip AltServer-$Arch
     echo "Done"
 elif [[ $LatestVersion = $LocalVersion ]] ; then
     echo "you are using the latest release"
