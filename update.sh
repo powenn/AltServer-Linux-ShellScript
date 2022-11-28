bcf049ab39bf3ba2d9b73c5fe578b9f44acf1faf
powenn <powen0966091716@gmail.com>
2022-11-28 12:48:35 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -21,17 +21,29 @@ if [[ $LatestVersion > $LocalVersion ]] ; then
 
 What updated in version $LatestVersion ?
   Script:
-    - Support wifi refresh (only x64 currently)
+    - Support wifi refresh 
     - Codes improved
   AltStore:
     - Updated to 1.5.1
   AltSerevr:
     - Fix Support for iOS 15
     - Updated to v0.0.5
-    - with netmuxd ,you can do wifi refresh (only x64 currently)
+    - with netmuxd ,you can do wifi refresh
   Others:
     - please make sure you have install libavahi-compat-libdnssd-dev
-    
+  Notice :
+    #########################################################################################
+    # https://github.com/osy/Jitterbug#pairing                                              #
+    #                                                                                       #
+    # download: https://github.com/osy/Jitterbug/releases                                   #
+    # Run the program in the zip of the OS you're using right now.                          #
+    #                                                                                       #
+    # Once you let it generate a pairing file, that .mobiledevicep file                     #
+    # then REPLACE the one in /var/lib/lockdown/ (where the UUID/numbers are the same!!)    #
+    #                                                                                       #
+    # This should only be done once, then wifi refresh should always work :)                #
+    #########################################################################################
+
 For more information: https://github.com/powenn/AltServer-Linux-ShellScript
 << PLease relaunch the script to apply the new version >>
 EOF
